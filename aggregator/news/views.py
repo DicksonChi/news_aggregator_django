2c5f5ffe1576100108e1e3063b597470d5d3d33b
DicksonChi <chibuzordick94@gmail.com>
2019-06-10 15:32:19 +0100

    fixed the feed parser error

--- aggregator/news/views.py
+++ aggregator/news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Article, Feed
 from .forms import FeedForm
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 import feedparser
 import datetime
@@ -32,11 +33,35 @@ def new_feed(request):
 
             # check if there is an existing feed with this url
             existing_feed = Feed.objects.filter(url=feed.url)
+            if len(existing_feed) > 0:
+                existing_feed_entries = Article.objects.filter(feed=existing_feed[0])
+                if len(existing_feed_entries) == 0:
+                    feed_data = feedparser.parse(feed.url)
+                    for entry in feed_data.entries:
+                        article = Article()
+                        article.title = entry.title
+                        article.url = entry.link
+                        article.description = entry.description
+
+                        date_string = datetime.datetime.now()  # init the time to now in case published_parse=False
+                        if entry.get('published_parse'):
+                            d = datetime.datetime(*(entry.published_parsed[0:6]))
+                            date_string = d.strftime('%Y-%m-%d %H:%M:%S')
+
+                        article.publication_date = date_string
+                        article.feed = existing_feed[0]
+                        article.save()
+
+                return HttpResponseRedirect(reverse('news:feeds_list'))
+
             if len(existing_feed) == 0:
                 feed_data = feedparser.parse(feed.url)
 
-                # set some fields
-                print(feed)
+                # return to the form if there is no error
+                if not feed.get('title') and len(feed.entries) == 0:
+                    form = FeedForm()
+                    return render(request, 'news/new_feed.html', {'form': form})
+
                 feed.title = feed_data.feed.title
                 feed.save()
 
@@ -53,7 +78,7 @@ def new_feed(request):
                     article.feed = feed
                     article.save()
 
-            return redirect('news.views.feeds_list')
+            return HttpResponseRedirect(reverse('news:feeds_list'))
     else:
         form = FeedForm()
     return render(request, 'news/new_feed.html', {'form': form})
